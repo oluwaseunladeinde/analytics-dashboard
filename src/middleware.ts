d4474ac23989f6a627195e8f7f4a08b342a0c4b1
oluwaseunladeinde <oluwaseun.ladeinde@yahoo.com>
2024-03-11 12:08:24 +0100

    first commit

---
+++ src/middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { analytics } from "./utils/analytics";
+
+export default async function middleware(req: NextRequest) {
+    if (req.nextUrl.pathname === '/') {
+        // track analytics event
+        try {
+            await analytics.track('pageview', {
+                page: '/',
+                country: req.geo?.country,
+            })
+        } catch (err) {
+            // fail silently to not affect request
+            console.error(err)
+        }
+    }
+
+    return NextResponse.next()
+}
+
+export const matcher = {
+    matcher: ['/'],
+}
